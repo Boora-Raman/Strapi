c2911dc83dac8aa4a63a53f11e5cffbbcd0fb43c
Boora-Raman <157504430+Boora-Raman@users.noreply.github.com>
2025-06-17 21:28:43 +0530

    Update ec2.tf

--- Terraform-Deployment/ec2/ec2.tf
+++ Terraform-Deployment/ec2/ec2.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "strapi_ec2" {
   instance_type               = "t2.small"
   subnet_id                   = var.subnet_id  
   vpc_security_group_ids      = [var.security_group_id]
- 
+  
   associate_public_ip_address = true
 
 
