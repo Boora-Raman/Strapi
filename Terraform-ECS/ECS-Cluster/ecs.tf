97faf6e962b4334f323646fa368815c358cccb04
Boora-Raman <booraraman2004@gmail.com>
2025-06-19 21:41:30 +0530

    ECS updated for Cloudwatch setup

--- Terraform-ECS/ECS-Cluster/ecs.tf
+++ Terraform-ECS/ECS-Cluster/ecs.tf
@@ -1,6 +1,13 @@
-resource "aws_ecs_cluster" "medusa-cluster" {
+resource "aws_ecs_cluster" "strapi-cluster" {
   name = "strapi-deployment-cluster"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+
   tags = {
     name = "strapi-deployment-cluster"
   }
-} 
+}
+
