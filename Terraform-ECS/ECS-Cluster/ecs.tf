3fe43d3f6616511493d4fa9b6b1aac3078eef6e0
Boora-Raman <157504430+Boora-Raman@users.noreply.github.com>
2025-06-23 09:41:38 +0530

    Update ecs.tf

--- Terraform-ECS/ECS-Cluster/ecs.tf
+++ Terraform-ECS/ECS-Cluster/ecs.tf
@@ -7,7 +7,17 @@ resource "aws_ecs_cluster" "medusa-cluster" {
   }
 
   tags = {
-    name = "strapi-deployment-cluster"
+    name = "Strapi-Deployment-ECS-workspace"
   }
 }
 
+resource "aws_ecs_cluster_capacity_providers" "strapi-cluster-capacity" {
+  cluster_name = aws_ecs_cluster.medusa-cluster.name
+
+  capacity_providers = ["FARGATE", "FARGATE_SPOT"]
+
+  default_capacity_provider_strategy {
+    capacity_provider = "FARGATE_SPOT"
+    weight            = 1
+  }
+}
