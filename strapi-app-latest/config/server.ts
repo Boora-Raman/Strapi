30c7a17c9a566b69c2af4e17ca631a0170fab42c
Boora-Raman <booraraman2004@gmail.com>
2025-06-24 17:00:33 +0530

    source code updated for strapi

--- strapi-app-latest/config/server.ts
+++ strapi-app-latest/config/server.ts
@@ -1,23 +1,7 @@
-// config/server.ts
-
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
-  // 👇 Add this block:
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET'),
-    },
-  },
-  // 👇 Here is the fix
-  allowedHosts: [
-    // 'strapi-test-lb-tf-1458014364.us-east-1.elb.amazonaws.com',
-    '*'
-  ],
 });
